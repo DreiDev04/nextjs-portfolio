0bcfe5a825fd2d0e0ca0096a13787d89bd3c84ff
John Andrei <tacujan.andre@gmail.com>
2024-07-03 02:59:38 +0800

    added: animation to main page and navbar mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,97 @@
+"use client";
 import React from "react";
-import { FlipWords } from "@/components/ui/flip-words";
 import Image from "next/image";
-import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
+
+const textVariants = {
+  hidden: { opacity: 0, y: 50 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+};
+
+const containerVariants = {
+  hidden: { opacity: 1 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.3,
+    },
+  },
+};
+
+const buttonVariants = {
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
+  hover: { scale: 1.1, transition: { yoyo: Infinity } },
+};
+
+const imageVariants = {
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
+  hover: { scale: 1.05, rotate: 2, transition: { duration: 0.5 } },
+};
 
 const Home = () => {
-  const words = ["student", "programmer", "freelancer"];
-
   return (
-    <div className="container flex flex-col-reverse md:flex-row justify-center md:px-14 mt-10 gap-4">
-
+    <motion.div
+      className="container flex flex-col-reverse md:flex-row justify-center md:px-14 mt-10 gap-4"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
       <div className="md:w-1/2 grid place-content-center container mb-10 md:mb-0">
-        <div className="text-4xl font-normal text-neutral-600 dark:text-neutral-400 ">
+        <motion.div
+          className="text-4xl font-normal text-neutral-600 dark:text-neutral-400"
+          variants={textVariants}
+        >
           Hello, I' am{" "}
           <span className="underline underline-offset-8 decoration-green-500">
             John Andrei
           </span>
-          {/* <br />
-          Im a <FlipWords words={words} /> <br /> */}
-        </div>
-        <p className="mt-5 md:w-2/3">
+        </motion.div>
+        <motion.p className="mt-5 md:w-2/3" variants={textVariants}>
           Based in the Philippines. I'm a NextJS/React Developer. Currently, a
           3rd year BS Computer Science Student. Passionate in making life easier
           through programming.
-        </p>
+        </motion.p>
         <div className="flex gap-2 mt-5">
-          <Button type="button"> View Projects</Button>
-          <Button variant="outline">Contact Me</Button>
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            whileHover="hover"
+            variants={buttonVariants}
+          >
+            <Button type="button">View Projects</Button>
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            whileHover="hover"
+            variants={buttonVariants}
+          >
+            <Button variant="outline">Contact Me</Button>
+          </motion.div>
         </div>
       </div>
-      <div className="md:w-1/2 grid place-content-center">
-      
-        <Image
-          src="/Profile_3.jpg"
-          height="400"
-          width="400"
-          className="object-cover rounded-2xl"
-          alt="me"
-          priority
-        />
+      <div className="md:w-1/2 grid place-content-center relative">
+        <motion.div
+          className="rounded-2xl relative"
+          initial="hidden"
+          animate="visible"
+          whileHover="hover"
+          variants={imageVariants}
+        >
+          <Image
+            src="/Profile_3.jpg"
+            height="400"
+            width="400"
+            className="object-cover rounded-2xl"
+            alt="me"
+            priority
+          />
+        </motion.div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,14 @@
 "use client";
 import React, { useState } from "react";
-import { FaFacebookSquare } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
+import { FaFacebookSquare, FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa6";
 import Link from "next/link";
 import ToggleSwitch from "@/components/custom/toggleDark";
-
 import { Button } from "@/components/ui/button";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoMdClose } from "react-icons/io";
 import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
 
 const socials = [
   {
@@ -47,9 +46,33 @@ const navLinks = [
   },
 ];
 
+const navVariant = {
+  hidden: {
+    opacity: 0,
+    y: "-100%",
+    transition: {
+      type: "spring",
+      stiffness: 100,
+      damping: 10,
+      duration: 0.2,
+    },
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 100,
+      damping: 10,
+      duration: 0.2,
+    },
+  },
+};
+
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   const pathname = usePathname();
+
   return (
     <nav className="flex flex-row justify-between container">
       <Link
@@ -59,14 +82,16 @@ const Navbar = () => {
         John Andrei 👨‍💻
       </Link>
 
-      {/*Desktop Navigation*/}
+      {/* Desktop Navigation */}
       <div className="hidden md:flex md:gap-6 lg:gap-10 xl:gap-14 ">
         <div className="flex gap-5">
           {navLinks.map((link, index) => (
             <Link
               href={link.link}
               key={index}
-              className={`text-lg font-medium px-2 rounded-lg ${pathname === link.link ? "bg-foreground text-background" : ""}`}
+              className={`text-lg font-medium px-2 rounded-lg ${
+                pathname === link.link ? "bg-foreground text-background" : ""
+              }`}
             >
               {link.name}
             </Link>
@@ -85,7 +110,7 @@ const Navbar = () => {
         </div>
       </div>
 
-      {/*Mobile Navigation*/}
+      {/* Mobile Navigation */}
       <div className="flex gap-10 md:hidden ">
         <div className="flex gap-1 items-center">
           <ToggleSwitch />
@@ -93,9 +118,7 @@ const Navbar = () => {
             variant="ghost"
             size="sm"
             className="z-50 relative "
-            onClick={() => {
-              setOpen((prev) => !prev);
-            }}
+            onClick={() => setOpen((prev) => !prev)}
           >
             {open ? (
               <IoMdClose className="text-lg text-background " />
@@ -104,31 +127,34 @@ const Navbar = () => {
             )}
           </Button>
         </div>
-        {open && (
-          <div className="absolute top-0 left-0 bg-foreground text-background w-full h-full flex flex-col justify-center px-10 gap-5 ">
-            <div className="flex gap-5 flex-col">
-              {navLinks.map((link, index) => (
-                <Link
-                  href={link.link}
-                  key={index}
-                  className="text-2xl font-medium px-2 rounded-lg "
-                >
-                  {link.name}
-                </Link>
-              ))}
-            </div>
-            <div className="flex gap-2 flex-row text-2xl justify-center">
-              {socials.map((social, index) => {
-                const Icon = social.icon;
-                return (
-                  <Link href={social.link} key={index} aria-label={social.name}>
-                    <Icon />
-                  </Link>
-                );
-              })}
-            </div>
+        <motion.div
+          className="absolute top-0 left-0 bg-foreground text-background w-full h-full flex flex-col justify-center px-10 gap-5"
+          variants={navVariant}
+          initial="hidden"
+          animate={open ? "visible" : "hidden"}
+        >
+          <div className="flex gap-5 flex-col">
+            {navLinks.map((link, index) => (
+              <Link
+                href={link.link}
+                key={index}
+                className="text-2xl font-medium px-2 rounded-lg "
+              >
+                {link.name}
+              </Link>
+            ))}
           </div>
-        )}
+          <div className="flex gap-2 flex-row text-2xl justify-center">
+            {socials.map((social, index) => {
+              const Icon = social.icon;
+              return (
+                <Link href={social.link} key={index} aria-label={social.name}>
+                  <Icon />
+                </Link>
+              );
+            })}
+          </div>
+        </motion.div>
       </div>
     </nav>
   );
